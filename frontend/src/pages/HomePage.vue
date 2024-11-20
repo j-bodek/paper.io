fcf13bd6e891f845c720ab79a9f0de4a607b02c1
Liljack118 <kamilzajaczek4@gmail.com>
2024-11-20 15:24:38 +0100

    add home, not found pages and style them

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,8 +1,14 @@
 
 <template>
-    <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the home page</p>
+    <div class="container">
+        <div class="wrapper">
+            <h1>Paper.io</h1>
+            <p>Create room and play with friend!</p>
+
+            <hr>
+
+            <button id="crateRoomBtn">Create Room</button>
+        </div>
     </div>
 </template>
 
@@ -10,12 +16,60 @@
 
 export default {
     name: 'HomePage',
-    data() {
-        return {}
+    mounted(){
+        this.createRoomButton.addEventListener('click', () => {
+            this.createRoom();
+        });
+    },
+    computed: {
+        createRoomButton(){
+            return document.getElementById('crateRoomBtn');
+        }
+    },
+    methods: {
+        createRoomUUID() {
+            // TODO: replace with api call to create room
+            return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+                (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+            );
+        },
+        createRoom() {
+            let uuid = this.createRoomUUID();
+            this.$router.push({ name: 'room', params: { uuid: uuid } });
+        }
     }
 }
 </script>
 
 <style scoped>
+.container {
+    background-color: #e9c46a;
+    width: 100%;
+    height: 100vh;
+    margin: 0 !important;
+    padding: 0 !important;
+    display: flex;
+}
+
+.wrapper {
+    margin: auto;
+    text-align: center;
+}
+
+hr {
+    width: 50%;
+    margin: 20px auto;
+    border: 1px solid black;
+}
+
+button {
+    background-color: #2a9d8f;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+}
 
 </style>
--- frontend/src/pages/NotFoundPage.vue
+++ frontend/src/pages/NotFoundPage.vue
@@ -1,7 +1,13 @@
 
 <template>
-    <div>
-        <h1>Not Found Page</h1>
+    <div class="container">
+        <div class="wrapper">
+            <h1>Not Found Page</h1>
+
+            <hr>
+
+            <button id="backToHomeBtn">Back To Home</button>
+        </div>
     </div>
 </template>
 
@@ -9,12 +15,53 @@
 
 export default {
     name: 'NotFoundPage',
-    data() {
-        return {}
+    mounted(){
+        this.backToHomeButton.addEventListener('click', () => {
+            this.backToHome();
+        });
+    },
+    computed: {
+        backToHomeButton(){
+            return document.getElementById('backToHomeBtn');
+        }
+    },
+    methods: {
+        backToHome() {
+            this.$router.push({ name: 'home' });
+        }
     }
 }
 </script>
 
 <style scoped>
+.container {
+    background-color: #e9c46a;
+    width: 100%;
+    height: 100vh;
+    margin: 0 !important;
+    padding: 0 !important;
+    display: flex;
+}
+
+.wrapper {
+    margin: auto;
+    text-align: center;
+}
+
+hr {
+    width: 50%;
+    margin: 20px auto;
+    border: 1px solid black;
+}
+
+button {
+    background-color: #2a9d8f;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+}
 
 </style>
